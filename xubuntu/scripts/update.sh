1951bcd2accc7a9c32e9b13a36a84dd4627bd896
Xuan-Thuy Dang <thuydang.de@gmail.com>
2019-01-29 20:38:48 +0100

    Split provisioning

--- xubuntu/scripts/update.sh
+++ xubuntu/scripts/update.sh
@@ -21,42 +21,4 @@ apt-get -y update;
 apt-get -y update
 apt-get -y upgrade
 
-# zgrandi: install Xubuntu without most extra packages
-apt-get	-y install --no-install-recommends xubuntu-desktop \
-	acpi-support \
-	apport-gtk \
-	curl \
-	desktop-file-utils \
-	evince \
-	file-roller \
-	firefox \
-	fonts-liberation \
-	gvfs \
-	indicator-application \
-	indicator-messages \
-	indicator-sound \
-	laptop-detect \
-	libcurl3 \
-	libnotify-bin \
-	light-locker \
-	lightdm-gtk-greeter-settings \
-	menulibre \
-	mousepad \
-	pavucontrol \
-	ristretto \
-	thunar-archive-plugin \
-	ttf-ubuntu-font-family \
-	update-notifier \
-	xfce4-datetime-plugin \
-	xfce4-indicator-plugin \
-	xfce4-mount-plugin \
-	xfce4-power-manager \
-	xfce4-quicklauncher-plugin \
-	xfce4-screenshooter \
-	xfce4-taskmanager \
-	xfce4-terminal \
-	xfce4-volumed \
-	xfce4-whiskermenu-plugin;
 
-reboot;
-sleep 60;
